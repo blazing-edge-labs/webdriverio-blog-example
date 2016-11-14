92ff7308a3a232644f1ed3350675ba61164d5f20
Alkaia <aprpic@gmail.com>
2016-11-14 17:51:00 +0100

    uprate .gitignore to ignore allure results folder

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,6 @@
 import gulp from 'gulp';
 import webdriver from 'gulp-webdriver';
 import selenium from 'selenium-standalone';
-import wdio from 'gulp-wdio';
 
 gulp.task('test', () => {
    return gulp.src('wdio.conf.js').pipe(webdriver());
