0770d1fef082a99fb29131931ab576036b708223
Alkaia <aprpic@gmail.com>
2016-11-17 23:06:34 +0100

    add npm scripts for cleanup and generate report after testing is done

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,13 @@
 import gulp from 'gulp';
 import webdriver from 'gulp-webdriver';
 
+//Test task
+//takes in a callback so the engine knows when the task's done
 gulp.task('test', () => {
-   return gulp.src('wdio.conf.js').pipe(webdriver());
+    return gulp.src('wdio.conf.js').pipe(webdriver());
+});
+
+// Watch Files For Changes
+gulp.task('watch', () => {
+    gulp.watch('./test/specs/*.js', ['test']);
 });
